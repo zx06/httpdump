f738ceb5e0c7f52322e301861677898d57d34b82
zx06 <12474586+zx06@users.noreply.github.com>
2023-06-19 22:26:39 +0800

    feat: add ui

--- main.go
+++ main.go
@@ -1,37 +1,130 @@
 package main
 
 import (
-	"flag"
-	"fmt"
-	"log"
-	"net/http/httputil"
+	"errors"
+	"io"
+	"math/rand"
+	"net/http"
+	"net/url"
+	"sync"
 
-	"github.com/gin-gonic/gin"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+
+	"github.com/zx06/httpdump/ui"
 )
 
-var port = "8080"
+const (
+	idKey = "id"
+)
 
-func init() {
-	gin.SetMode(gin.ReleaseMode)
-	flag.StringVar(&port, "p", "8080", "port to listen on")
+type Record struct {
+	Method     string      `json:"method"`
+	URL        url.URL     `json:"url"`
+	Proto      string      `json:"proto"`
+	Headers    http.Header `json:"headers"`
+	Body       []byte      `json:"body"`
+	Host       string      `json:"host"`
+	RemoteAddr string      `json:"remote_addr"`
 }
 
-func handler(c *gin.Context) {
-	d, err := httputil.DumpRequest(c.Request, true)
-	if err != nil {
-		msg := fmt.Sprintf("couldn't dump request: %s", err)
-		log.Printf(msg)
-		c.String(500, "%s", err)
-		return
+var (
+	store = make(map[string][]Record)
+	mu    sync.RWMutex
+)
+
+func genID() string {
+	// generate random id in a-z
+	var (
+		id     string
+		chars  = []rune("abcdefghijklmnopqrstuvwxyz")
+		length = 10
+	)
+	for i := 0; i < length; i++ {
+		id += string(chars[rand.Intn(len(chars))])
 	}
-	b := string(d)
-	log.Printf("\nrequest received:\n%s\n", b)
-	c.String(200, b)
+	return id
+}
+
+func initCookieMiddleware() echo.MiddlewareFunc {
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			_, err := c.Cookie(idKey)
+			if err != nil {
+				if errors.Is(err, http.ErrNoCookie) {
+					id := genID()
+					cookie := &http.Cookie{
+						Name:  idKey,
+						Value: id,
+						Path:  "/",
+					}
+					c.SetCookie(cookie)
+				} else {
+					return err
+				}
+			}
+			return next(c)
+		}
+	}
 }
 
 func main() {
-	flag.Parse()
-	router := gin.Default()
-	router.Any("/", handler)
-	router.Run(":" + port)
+	app := echo.New()
+	app.Use(middleware.Logger())
+	staticConfig := middleware.StaticConfig{
+		Root:       "dist",
+		HTML5:      true,
+		IgnoreBase: true,
+		Filesystem: http.FS(ui.Dist),
+	}
+	app.Use(middleware.StaticWithConfig(staticConfig))
+	app.Any("/x/:id/*", func(c echo.Context) error {
+		id := c.Param("id")
+		if id == "" {
+			return c.String(http.StatusBadRequest, "id is required")
+		}
+		bb, err := io.ReadAll(c.Request().Body)
+		if err != nil {
+			return c.String(http.StatusInternalServerError, err.Error())
+		}
+		// write to store
+		mu.Lock()
+		defer mu.Unlock()
+		r := Record{
+			Method:     c.Request().Method,
+			URL:        *c.Request().URL,
+			Proto:      c.Request().Proto,
+			Headers:    c.Request().Header,
+			Body:       bb,
+			Host:       c.Request().Host,
+			RemoteAddr: c.Request().RemoteAddr,
+		}
+		store[id] = append(store[id], r)
+		return c.JSON(http.StatusOK, r)
+	})
+	api := app.Group("/api")
+	api.Use(initCookieMiddleware())
+	{
+		apiRecord := api.Group("/record")
+		{
+			apiRecord.GET("/", func(c echo.Context) error {
+				mu.RLock()
+				defer mu.RUnlock()
+				return c.JSON(http.StatusOK, store)
+			})
+			apiRecord.GET("/:id", func(c echo.Context) error {
+				id := c.Param("id")
+				if id == "" {
+					return c.String(http.StatusBadRequest, "id is required")
+				}
+				mu.RLock()
+				defer mu.RUnlock()
+				return c.JSON(http.StatusOK, store[id])
+			})
+		}
+	}
+	err := app.Start(":1234")
+	if err != nil {
+		panic(err)
+	}
 }
---
+++ ui/src/lib/Records.svelte
@@ -0,0 +1,21 @@
+<script>
+    let records = {};
+    // fetch every 5 seconds
+    function fetchRecords() {
+        fetch("/api/record/")
+            .then((response) => response.json())
+            .then((data) => {
+                records = data;
+            });
+    }
+    fetchRecords();
+    setInterval(() => {
+        fetchRecords()
+    }, 5000);
+</script>
+
+<ul>
+    {#each Object.keys(records) as key}
+        <li>id: {key},len: {records[key].length}</li>
+    {/each}
+</ul>
